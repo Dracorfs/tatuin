e5f4a4ca078585d1019b986baefae85da87ac17f
Dracorfs <franciscosantopietro@hotmail.com>
2023-08-05 23:24:46 -0300

    ADD: tailwind

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import tw from 'twrnc'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={tw`flex-1 justify-center items-center bg-black`}>
+      <Text style={tw`text-white text-3xl font-bold`}>tatuin</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
