1d1d7c6723bd7974eb4844647cfa37759de0d42e
Dracorfs <franciscosantopietro@hotmail.com>
2023-09-13 21:18:43 -0300

    DELETE: tailwind

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
-import tw from 'twrnc'
 
 export default function App() {
   return (
-    <View style={tw`flex-1 justify-center items-center bg-black`}>
-      <Text style={tw`text-white text-3xl font-bold`}>tatuin</Text>
+    <View>
+      <Text>tatuin</Text>
       <StatusBar style="auto" />
     </View>
   );
