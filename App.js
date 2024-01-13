3bb40aea5df1ff0b76ff6fb9976310c128e875ba
Dracorfs <franciscosantopietro@hotmail.com>
2024-01-13 16:56:21 -0300

    Revert "DELETE: tailwind"
    This reverts commit 1d1d7c6723bd7974eb4844647cfa37759de0d42e.

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import tw from 'twrnc'
 
 export default function App() {
   return (
-    <View>
-      <Text>tatuin</Text>
+    <View style={tw`flex-1 justify-center items-center bg-black`}>
+      <Text style={tw`text-white text-3xl font-bold`}>tatuin</Text>
       <StatusBar style="auto" />
     </View>
   );
