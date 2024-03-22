f3472b16df6a06df29c0326d21f008fa66490386
Dracorfs <franciscosantopietro@hotmail.com>
2024-03-22 00:48:51 -0300

    add google maps

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>tatuin</Text>
       <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: -34.6037389,
+          longitude: -58.3815704
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: -34.6037389,
+            longitude: -58.3815704
+          }}
+          title={"El Obelisco"}
+          description={"El monumento de Porteñolandia"}
+        />
+      </MapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
